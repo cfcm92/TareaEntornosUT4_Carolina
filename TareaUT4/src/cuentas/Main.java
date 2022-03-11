b7d533b6f32612d877798fb4b1882133cf24594f
carolina <carolina@ubuntu>
2022-03-11 10:21:46 +0100

    Añadir el método operativa_cuenta

--- TareaUT4/src/cuentas/Main.java
+++ TareaUT4/src/cuentas/Main.java
@@ -6,7 +6,12 @@ public class Main {
         double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
+        operativa_cuenta(cuenta1);
+    }
+
+	private static void operativa_cuenta(CCuenta cuenta1) {
+		double saldoActual;
+		saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
@@ -20,5 +25,5 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-    }
+	}
 }
