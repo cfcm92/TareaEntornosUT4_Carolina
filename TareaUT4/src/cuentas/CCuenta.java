5a569dd75cae174b85e5b792f236e1793d03592f
carolina <carolina@ubuntu>
2022-03-11 11:56:38 +0100

    Añadido de comentarios para completar Javadoc

--- TareaUT4/src/cuentas/CCuenta.java
+++ TareaUT4/src/cuentas/CCuenta.java
@@ -1,4 +1,15 @@
+/**
+ * @autor Carolina Fernández
+ * version 1.0
+ * since 11/03/2022
+ * 
+ */
 package cuentas;
+/**
+ * Clase para la creación de una cuenta bancaria. Incluye los métodos ingresar, 
+ * retirar y estado, además de los getter y setter.
+ *
+ */
 public class CCuenta {
 
 
@@ -22,14 +33,22 @@ public class CCuenta {
     {
         return getSaldo();
     }
-
+/**
+ * Método para ingresar dinero a una cuenta bancaria
+ * @param cantidad la cantidad que quieres ingresar
+ * @throws Exception cuando ingresa una cantidad negativa
+ */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+/**
+ * Método para retirar dinero de una cuenta bancaria
+ * @param cantidad cantidad que quieres retirar
+ * @throws Exception Cuando no hay suficiente saldo o si has ingresado una cantidad negativa
+ */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,34 +58,66 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /**
+     * Obtienes el saldo de la cuenta
+     * @return saldo
+     */
 	protected double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * Establecer el saldo de la cuenta
+	 * @param saldo saldo que tiene la cuenta
+	 */
 	protected void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * Te dice el número de cuenta
+	 * @return número de cuenta
+	 */
 	private String getCuenta() {
 		return cuenta;
 	}
 
+	/** 
+	 * Establecer el número de cuenta
+	 * @param cuenta número de cuenta
+	 */
 	private void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * Obtener el nombre del titular de la cuenta
+	 * @return nombre del titular de la cuenta
+	 */
 	private String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Establecer el nombre del titular de la cuenta
+	 * @param nombre nombre del titular de la cuenta
+	 */
 	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Devuelve qué tipo de interés va a tener la cuenta establecido en número
+	 * @return tipo de inteŕes de tipo double
+	 */
 	private double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	/**
+	 * Establecer tipo de interés de la cuenta (en número)
+	 * @param tipoInterés tipo de interés de la cuenta de tipo double
+	 */
 	private void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
--- TareaUT4/src/cuentas/Main.java
+++ TareaUT4/src/cuentas/Main.java
@@ -1,14 +1,27 @@
 package cuentas;
+/**
+ * Método para probar la clase CCuenta
+ * 
+ */
 public class Main {
-
+/**
+ * Iniciamos una cuenta y operamos con ella
+ * @param args 
+ */
     public static void main(String[] args) {
-        CCuenta cuenta1;
+        
+    	CCuenta cuenta1;
         double saldoActual = 0;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         operativa_cuenta(cuenta1, saldoActual);
     }
 
+    /**
+     * Método con las operaciones relativas a una instancia de CCuenta
+     * @param cuenta1 objeto de tipo CCuenta
+     * @param cantidad el saldo con el que se va a iniciar la cuenta
+     */
 	private static void operativa_cuenta(CCuenta cuenta1, double cantidad) {
 
 		cuenta1.setSaldo(cantidad);
